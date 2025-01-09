3b8fda57cc2c6281bfbd9e9511c0c83f88fe45ef
cxc1920 <83142680+cxc1920@users.noreply.github.com>
2025-01-09 12:18:39 -0500

    Add files via upload

---
+++ 03_lab/Lab6/Lab6_template.py
@@ -0,0 +1,19 @@
+###############################################################################
+# Provide the essential information of your team
+################################################################################
+Email1 = "aaa123@miami.edu" # change the email to your email
+Email2 = "bbb456@miami.edu" # change the email to your email
+Group_num = 33              # provide your group number
+Assignment = "Lab 6"
+################################################################################
+print('==========Q1==========')
+# Exercise 1. Write your code for exercise 1 below.
+
+
+
+
+
+
+
+
+
